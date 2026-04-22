bdd0082c516793fc06b3cdfd09cd6961a328c2c8
Tomas Vink <tomasvink@gmail.com>
2026-04-22 10:14:48 +0200

    update html templates

--- src/routes/cta-button/template.ts
+++ src/routes/cta-button/template.ts
@@ -5,21 +5,33 @@ export function buildEmailHTML(
   width: number,
   outlookWidth: number
 ): string {
-  return `<div style="margin:20px 0px;">
-<table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation"><tr><td align="center">
-<table border="0" cellspacing="0" cellpadding="0" role="presentation"><tr><td>
-<!--[if mso]>
-  <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word"
-    href="${url}"
-    style="height:45px;v-text-anchor:middle;width:${outlookWidth}px;" arcsize="20%" stroke="f" fillcolor="${color}">
-  <w:anchorlock/>
-  <center>
-<![endif]-->
-<a href="${url}"
-   style="background-color:${color};border-radius:8px;color:#ffffff;display:inline-block;font-family:Verdana,sans-serif;font-size:16px;font-weight:bold;line-height:45px;text-align:center;text-decoration:none;width:${width}px;-webkit-text-size-adjust:none;">${title}</a>
-<!--[if mso]>
-  </center>
-  </v:roundrect>
-<![endif]-->
-</td></tr></table></td></tr></table></div>`
+  return `
+<div style="margin:20px 0px;">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation">
+    <tr>
+      <td align="center">
+        <table border="0" cellspacing="0" cellpadding="0" role="presentation">
+          <tr>
+            <td>
+              <!--[if mso]>
+                <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word"
+                  href="${url}"
+                  style="height:45px;v-text-anchor:middle;width:${outlookWidth}px;" arcsize="20%" stroke="f" fillcolor="${color}">
+                <w:anchorlock/>
+                <center>
+              <![endif]-->
+              <a href="${url}"
+                style="background-color:${color};border-radius:8px;color:#ffffff;display:inline-block;font-family:Verdana,sans-serif;font-size:16px;font-weight:bold;line-height:45px;text-align:center;text-decoration:none;width:${width}px;-webkit-text-size-adjust:none;">${title}</a>
+              <!--[if mso]>
+                </center>
+                </v:roundrect>
+              <![endif]-->
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</div>
+`
 }
--- src/routes/title/template.ts
+++ src/routes/title/template.ts
@@ -1,21 +1,29 @@
 export function buildEmailHTML(anchor: string, title: string, color: string): string {
   return `
-<table cellpadding="0" cellspacing="0" width="100%" role="presentation" class="stylingblock-content-wrapper" style="min-width: 100%; "><tr><td class="stylingblock-content-margin-cell" style="padding: 12px 0px; "><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="background-color: ${color}; min-width: 100%; " class="stylingblock-content-wrapper"><tr><td style="padding: 5px 20px; " class="stylingblock-content-wrapper camarker-inner"><a id="${anchor}" name="${anchor}"></a>
+<table cellpadding="0" cellspacing="0" width="100%" role="presentation"
+  style="background-color: ${color}; min-width: 100%; " class="stylingblock-content-wrapper">
+  <tr>
+    <td style="padding: 5px 20px; " class="stylingblock-content-wrapper camarker-inner"><a id="${anchor}"
+        name="${anchor}"></a>
 
-<!--[if mso]>
-<table role="presentation" border="0" cellpadding="12" cellspacing="0" width="100%">
-<tr>
-<td style="color:#181818;font-family:Verdana, Geneva, sans-serif;font-size:23px;font-weight:bold;line-height:1.5;mso-line-height-rule:exactly;">
-<span style="color:#ffffff;">${title}</span>
-</td>
-</tr>
+      <!--[if mso]>
+      <table role="presentation" border="0" cellpadding="12" cellspacing="0" width="100%">
+      <tr>
+      <td style="color:#181818;font-family:Verdana, Geneva, sans-serif;font-size:23px;font-weight:bold;line-height:1.5;mso-line-height-rule:exactly;">
+      <span style="color:#ffffff;">${title}</span>
+      </td>
+      </tr>
+      </table>
+      <![endif]-->
+
+      <!--[if !mso]><!-- -->
+      <h1
+        style="color:#181818;font-family:Verdana, Geneva, sans-serif;font-size:23px;font-style:normal;font-weight:bold;line-height:150%;">
+        <span style="color:#ffffff;">${title}</span>
+      </h1>
+      <!--<![endif]-->
+    </td>
+  </tr>
 </table>
-<![endif]-->
-
-<!--[if !mso]><!-- -->
-<h1 style="color:#181818;font-family:Verdana, Geneva, sans-serif;font-size:23px;font-style:normal;font-weight:bold;line-height:150%;">
-<span style="color:#ffffff;">${title}</span>
-</h1>
-<!--<![endif]--></td></tr></table></td></tr></table>
 `
 }
